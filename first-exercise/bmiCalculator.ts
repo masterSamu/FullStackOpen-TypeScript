fe82d3dc0c3c09b26a6e074888208a2799f59257
Samu <samu.uunonen@student.hamk.fi>
2022-06-07 19:52:02 +0300

    exercises 9.-9.3

---
+++ first-exercise/bmiCalculator.ts
@@ -0,0 +1,42 @@
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi = (weight / height ** 2) * 10000;
+  const underweight = bmi < 18.5;
+  const normal = bmi >= 18.5 && bmi < 25;
+  const overweight = bmi >= 25 && bmi < 30;
+  const obese = bmi >= 30 && bmi < 35;
+  const extreme = bmi > 35;
+
+  if (underweight) {
+    return "Underweight";
+  } else if (normal) {
+    return "Normal (healthy weight)";
+  } else if (overweight) {
+    return "Overweight";
+  } else if (obese) {
+    return "Obese";
+  } else if (extreme) {
+    return "Extremely obese";
+  } else {
+    throw new Error("Could not calculate BMI");
+  }
+};
+
+try {
+  const args: Array<string> = process.argv;
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+  if (isNaN(Number(args[2])) && isNaN(Number(args[3]))) {
+    throw new Error("False arguments");
+  } else {
+    const height = Number(args[2]);
+    const weight = Number(args[3]);
+
+    console.log(calculateBmi(height, weight));
+  }
+} catch (error) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
---
+++ first-exercise/exerciseCalculator.ts
@@ -0,0 +1,94 @@
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: 0 | 1 | 2 | 3 | null;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+type Rating = 0 | 1 | 2 | 3 | null;
+
+const getArguments = (): Array<string> => {
+  const args: Array<string> = process.argv;
+  if (args.length < 4) throw new Error("Not enough arguments");
+  return args;
+};
+
+const calculateExercises = (data: Array<number>, target: number): Result => {
+  const periodLength: number = data.length;
+  const average: number = data.reduce((a, b) => a + b, 0) / periodLength;
+  const trainingDays = data.filter((hours) => hours > 0).length;
+  const success: boolean = average >= target;
+  const rating: Rating = getRating(average, target);
+  const ratingDescription: string = getDescription(rating);
+
+  const object = {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+  return object;
+};
+
+const getRating = (average: number, target: number): Rating => {
+  switch (true) {
+    case average >= target:
+      return 3;
+    case average >= target / 2:
+      return 2;
+    case average >= target / 3:
+      return 1;
+    case average < target / 3:
+      return 0;
+    default:
+      throw new Error("Could not calculate rating.");
+  }
+};
+
+const getDescription = (rate: Rating): string => {
+  switch (rate) {
+    case 3:
+      return "Well done!";
+    case 2:
+      return "Good job, but could do better!";
+    case 1:
+      return "Bad job, you must do better!";
+    case 0:
+      return "Terrible, just terrible..";
+    default:
+      throw new Error(
+        "Rating is not correct number, could not get the description!"
+      );
+  }
+};
+
+try {
+  const args: Array<string> = getArguments();
+
+  // Last item of the arguments is the target value
+  const target: number = Number(args[args.length - 1]);
+  const values: Array<number> = args.reduce(
+    (values, currentValue, currentIndex) => {
+      if (currentIndex > 1 && currentIndex < args.length - 1) {
+        values.push(Number(currentValue));
+      }
+      return values;
+    },
+    []
+  );
+
+  const result: Result = calculateExercises(values, target);
+  console.log(result);
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
